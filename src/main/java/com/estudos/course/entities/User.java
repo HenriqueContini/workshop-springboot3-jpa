7f555dfec993366a4dacba53a5f971054c15c297
HenriqueContini <henrique.coutine@gmail.com>
2023-12-18 14:24:49 -0300

    H2 database, test profile, JPA

--- src/main/java/com/estudos/course/entities/User.java
+++ src/main/java/com/estudos/course/entities/User.java
@@ -1,9 +1,13 @@
 package com.estudos.course.entities;
 
-import java.io.Serializable;
+import jakarta.persistence.*;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
